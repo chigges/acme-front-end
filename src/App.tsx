c20ce0d70863c5fb89679e6e8d0c97c6898f43bd
CtrlAltDelight <git+git@ctrlaltdelight.org>
2023-12-05 15:37:37 -0500

    Added one small thing to fix issue with loading pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,36 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useEffect } from "react";
-import './App.css'
+import "./App.css";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import Packages from "./pages/Packages";
+import Login from "./pages/Login";
 
 function App() {
-	// Set title of page
-	useEffect(() => {
-		document.title = "ACME Site";
-	}, []);
+  // Set title of page
+  useEffect(() => {
+    document.title = "ACME Site";
+  }, []);
 
-	return (
-	<>
-		<Router>
-			<div className="dark-theme">
-				<Routes>
-					<Route path="/" element={<Layout />}>
-						<Route index element={<Home />} />
-						<Route path="/packages" element={<Packages />} />
-					</Route>
-				</Routes>
-			</div>
-		</Router>
-	</>
-	);
-	// Self closing syntax: <Message />
+  return (
+    <>
+      <Router>
+        <div className="dark-theme">
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path="/packages" element={<Packages />} />
+              <Route
+                path="/login"
+                element={<Login onLogin={() => console.log("logged in!")} />}
+              />
+            </Route>
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
+  // Self closing syntax: <Message />
 }
 
 export default App;
