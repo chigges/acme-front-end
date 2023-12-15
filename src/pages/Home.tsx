f8ee54d8d619cde8941e9e3e6140f9b473efe091
CtrlAltDelight <lbchigges@gmail.com>
2023-12-14 20:04:20 -0500

    Fixed build error

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,20 @@
-import { NavLink } from "react-router-dom";
-
 function Home() {
-	return (
-		<>
-			<div id="image-backing" className="image-backing" style={{
-				minHeight: "100vh",
-			}}>
-				<section>
-					<h1 className="text-4xl">Overview</h1>
-					<p>This system is meant to </p>
-				</section>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div
+        id="image-backing"
+        className="image-backing"
+        style={{
+          minHeight: "100vh",
+        }}
+      >
+        <section>
+          <h1 className="text-4xl">Overview</h1>
+          <p>This system is meant to </p>
+        </section>
+      </div>
+    </>
+  );
 }
 
 export default Home;
